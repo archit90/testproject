ebf934993c9f9a6304eba3994993d64bf2bf1e3b
archit90 <a.gupta.archit@gmail.com>
2014-08-27 14:37:55 +0530

    added comment block for TestSync1

--- src/synchronization/TestSync1.java
+++ src/synchronization/TestSync1.java
@@ -1,10 +1,21 @@
 package synchronization;
 
+/**
+ * The A class has two methods that are synchronized. <br/>
+ * Threads B, C are passed the same object of class A <br/>
+ * and methods on A are called. <br/>
+ * Only one thread can access the function test1 or test2. <br/>
+ * <p/>
+ * The lock is on the object of A.<br/>
+ * If one thread enters a synchronized method of A it locks the object A for itself, <br/>
+ * no other thread can access the any synchronized methods of A <br/>
+ * al long as the synchronized method of A finishes <br/>
+ */
 public class TestSync1 {
   public static void main(String[] args) {
-    A a1=new A();
-    B b1 =new B(a1);
-    C c1=new C(a1);
+    A a1 = new A();
+    B b1 = new B(a1);
+    C c1 = new C(a1);
     b1.start();
     c1.start();
   }
