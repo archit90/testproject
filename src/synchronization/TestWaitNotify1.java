9e1aa0aa274764e63e1831f4ee649d4334e23b67
archit90 <a.gupta.archit@gmail.com>
2014-08-28 14:59:15 +0530

    Added a test for wait() and notify() added notifyAll

--- src/synchronization/TestWaitNotify1.java
+++ src/synchronization/TestWaitNotify1.java
@@ -1,7 +1,13 @@
 package synchronization;
 
 public class TestWaitNotify1 {
+
   public static void main(String[] args) {
+    main1(args); // wait and notify
+    main2(args); // wait ands notifyAll
+  }
+
+  public static void main1(String[] args) {
     TA a1 = new TA();
     Thr1 t1 = new Thr1(a1);
     Thr1 t2 = new Thr1(a1);
@@ -16,6 +22,21 @@ public class TestWaitNotify1 {
     a1.test2(); // releases one of the threads
     a1.test2(); // releases the other thread
     a1.test2(); // does not make a difference
+  }
+
+  public static void main2(String[] args) {
+    TA a1 = new TA();
+    Thr1 t1 = new Thr1(a1);
+    Thr1 t2 = new Thr1(a1);
+    t1.start();
+    t2.start();
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("About to release");
+    a1.test3(); // releases all the threads
   }
 }
 
@@ -34,6 +55,10 @@ class TA {
 
   synchronized void test2() {
     notify();
+  }
+
+  synchronized void test3() {
+    notifyAll();
   }
 
 }
